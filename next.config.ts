aaa70f53c777ae0782b22830f05cdea0a112c2bc
FatB0YY <rodion.rm111@icloud.com>
2025-06-17 10:25:37 +0300

    [T-3] Bugfix. Project. Changed bundle analyze.

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
 import type { NextConfig } from 'next';
 import StylelintPlugin from 'stylelint-webpack-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import WebpackBar from 'webpackbar';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   webpack(config) {
     config.plugins.push(new StylelintPlugin());
     config.plugins.push(new WebpackBar());
 
-    if (process.env.ANALYZE === 'true') {
-      config.plugins.push(new BundleAnalyzerPlugin());
-    }
-
     return config;
   },
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
